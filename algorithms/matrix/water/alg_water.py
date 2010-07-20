0fc5c485d1d33393aa1a690a4c1602699b95a864
Aleksey Komissarov <ad3002@gmail.com>
2010-07-21 03:08:23 +0400

    more tests

--- algorithms/matrix/water/alg_water.py
+++ algorithms/matrix/water/alg_water.py
@@ -384,12 +384,24 @@ if __name__ == '__main__':
     t = Timer("test(gr)", "from __main__ import test; from __main__ import gr")
     print "Grebenka 100x100 case: ", t.timeit(number=1)
     
+    gr = gen_grebenka(1000,1000)
+    t = Timer("test(gr)", "from __main__ import test; from __main__ import gr")
+    print "Grebenka 1000x1000x501 case: ", t.timeit(number=1)
+    
     s = get_spiral()
     t = Timer("test(s)", "from __main__ import test; from __main__ import s")
     print "Spiral case 1000x1000: ", t.timeit(number=1)
     
     # тестим зависимость от размера
     
+    for k in [2,3,5,10]:
+        a = numpy.random.rand(1000,1000)
+        N,M = a.shape
+        for i in range(0,N):
+            for j in range(0,M):
+                a[i,j] = add_height(a[i,j], k) 
+        t = Timer("test(a)", "from __main__ import test;from __main__ import a")
+        print "Random 1000x1000x%s: " % (k), t.timeit(number=1)   
     
    
 #    
