907cf5745a8fd4ae82a205b7c2dbebefeebbc80a
Aleksey Komissarov <ad3002@gmail.com>
2013-10-09 20:42:50 +0400

    function for freq computation

--- jellyfish/compute_kmer_coverage.py
+++ jellyfish/compute_kmer_coverage.py
@@ -16,5 +16,19 @@ def compute_histogram(data_tab_file):
 		for line in fh:
 			data = line.strip().split("\t")
 			D[int(data[1])] += 1
+	return D
 
+def compute_kmers_freq(D, output_file):
+	'''
+	'''
+	total = 0.0
+	for x, y in lines:
+	    D[y] += x
+	    total += x
+	D = D.items()
+	D.sort()
+	with open(output_file, "w") as fh:
+	    for k, v in D:
+	        s = "%s\t%s\n" % (k, round(100*v/total, 3))
+	        fh.write(s)
 
