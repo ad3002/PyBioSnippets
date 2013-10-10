b9286360bdefc664ac3815e047da2dd45d213f22
Aleksey Komissarov <ad3002@gmail.com>
2013-10-10 16:23:59 +0400

    join hiseq fiels

---
+++ hiseq/join_fastqs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#@created: 10.10.2013
+#@author: Aleksey Komissarov
+#@contact: ad3002@gmail.com
+'''
+Join fragmented fastq files from HiSeq.
+'''
+
+import os
+from collections import defaultdict
+
+def join_hiseq_files(folder):
+	'''
+	'''
+	join_groups = defaultdict(list)
+	for root, dirs, files in os.walk(folder, topdown=False):
+            for name in files:
+                if re.search(self.mask, name):
+                    apath = os.path.join(root, name)
+                    name_parts = name.split("_")
+                    similar_part = "_".join(name[:4])
+                    join_groups[similar_part].append(apath)
+    print join_groups
