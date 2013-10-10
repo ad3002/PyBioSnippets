a9725e6a0711e76ea8fe427fec5bfd6e3f90cf80
Aleksey Komissarov <ad3002@gmail.com>
2013-10-10 17:29:40 +0400

    separate_reads_witn_n_and_sharps

--- hiseq/fastq_tools.py
+++ hiseq/fastq_tools.py
@@ -20,3 +20,26 @@ def fix_uncorrect_long_quality(fastq_file, corrected_fastq_output):
 				print
 				print read.fastq
 			fh.write(read.fastq)
+
+def separate_reads_witn_n_and_sharps(fastq_file, output_file, reads_with_n_file, reads_with_sharp_file):
+	''' Separate reads from fastq file witn N and sharp quality scores.
+	'''
+	reads = []
+	print "Read data..."
+	k = 0
+	s = 0
+	with open(output_file, "w") as good_fh:
+		with open(reads_with_n_file, "w") as bad_fh:
+			with open(reads_with_sharp_file, "w") as sharp_fh:
+				for i, read in enumerate(fastq_reader(fastq_file)):
+					if "n" in read.sequence or "N" in read.sequence:
+						k += 1 
+						print i, k, s, float(k+1)/(i+1), float(s+1)/(i+1), "\r",
+						bad_fh.write(read.fastq)
+					elif "#" in read.qual:
+						s += 1
+						print i, k, s, float(k+1)/(i+1), float(s+1)/(i+1), "\r",
+						sharp_fh.write(read.fastq)
+					else:
+						good_fh.write(read.fastq)
+	print
