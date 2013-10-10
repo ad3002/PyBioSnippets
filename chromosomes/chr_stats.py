45108ede7e344209291fafdcbe033b0e2a05d57a
Aleksey Komissarov <ad3002@gmail.com>
2013-10-10 14:35:45 +0400

    get_chromosome_lengths function

---
+++ chromosomes/chr_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#@created: 09.10.2013
+#@author: Aleksey Komissarov
+#@contact: ad3002@gmail.com
+
+from trseeker.seqio.fasta_file import sc_iter_fasta
+
+def get_chromosome_lengths(rerence_multifasta):
+    ''' Get chromosome lengths.
+    @param rerence_multifasta: multifasta file with chromosomes
+    @return: dictionary chr name -> chr length
+    '''
+    print "Read reference genome"
+    chrs = {}
+    for seq_obj in sc_iter_fasta(rerence_multifasta):
+        chrs[seq_obj.seq_gi] = seq_obj.seq_length 
+        chrs[seq_obj.seq_gi] = seq_obj.seq_length
+    print chrs
