4fbde9305a589633bf7cad87fd6c3754a70eb990
Aleksey Komissarov <ad3002@gmail.com>
2013-10-10 16:27:53 +0400

    intendation error

--- hiseq/join_fastqs.py
+++ hiseq/join_fastqs.py
@@ -16,13 +16,13 @@ def join_hiseq_files(folder):
 	'''
 	join_groups = defaultdict(list)
 	for root, dirs, files in os.walk(folder, topdown=False):
-            for name in files:
-                if re.search(self.mask, name):
-                    apath = os.path.join(root, name)
-                    name_parts = name.split("_")
-                    similar_part = "_".join(name[:4])
-                    join_groups[similar_part].append(apath)
-    print join_groups
+		for name in files:
+			if re.search(self.mask, name):
+				apath = os.path.join(root, name)
+				name_parts = name.split("_")
+				similar_part = "_".join(name[:4])
+				join_groups[similar_part].append(apath)
+	print join_groups
 
 if __name__ == '__main__':
 	if len(sys.argv) != 2:
