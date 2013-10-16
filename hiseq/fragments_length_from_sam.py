9bb9e2ce82c74aa7b06f57df27075ddfd27fd38f
Aleksey Komissarov <ad3002@gmail.com>
2013-10-16 22:39:01 +0400

    compute fragment lengths

---
+++ hiseq/fragments_length_from_sam.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#@created: 29.01.2013
+#@author: Aleksey Komissarov
+#@contact: ad3002@gmail.com 
+
+from trseeker.seqio.sam_file import sc_sam_reader
+
