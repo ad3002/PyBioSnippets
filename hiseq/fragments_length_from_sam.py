9225eae086bf83345023ada438fdb50711687d02
Aleksey Komissarov <ad3002@gmail.com>
2013-10-16 22:49:27 +0400

    command line args

--- hiseq/fragments_length_from_sam.py
+++ hiseq/fragments_length_from_sam.py
@@ -8,6 +8,7 @@
 from trseeker.seqio.sam_file import sc_sam_reader
 from collections import defaultdict
 from trseeker.tools.draw_tools import draw_distribution_plot
+import argparse
 
 def compute_fragments_statistics(settings):
     '''
@@ -20,4 +21,12 @@ def compute_fragments_statistics(settings):
     draw_distribution_plot(lengths, image_file)
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Compute fragments from SAM file.')
+    parser.add_argument('-o','--ouput', help='Output image', required=True)
+    parser.add_argument('-i','--input', help='Input SAM file', required=True)
+    args = vars(parser.parse_args())
+    settings = {
+        "sam_file": args["input"],
+        "image_file": args["ouput"],
+    }
     compute_fragments_statistics(settings)
