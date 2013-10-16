ff74ca8701ea7e92bc6b9a26cabc53d028cc2f60
Aleksey Komissarov <ad3002@gmail.com>
2013-10-16 22:46:30 +0400

    plot drawing added

--- hiseq/fragments_length_from_sam.py
+++ hiseq/fragments_length_from_sam.py
@@ -7,14 +7,17 @@
 
 from trseeker.seqio.sam_file import sc_sam_reader
 from collections import defaultdict
+from trseeker.tools.draw_tools import draw_distribution_plot
 
 def compute_fragments_statistics(settings):
     '''
     '''
     sam_file = settings["sam_file"]
+    image_file = settings["image_file"]
     lengths = defaultdict(int)
     for sam_obj in sc_sam_reader(sam_file):
          lengths[sam_obj.fragment_length] += 1
+    draw_distribution_plot(lengths, image_file)
 
 if __name__ == '__main__':
     compute_fragments_statistics(settings)
