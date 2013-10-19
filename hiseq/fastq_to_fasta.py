c2c72defb5016d237fcfd919c1bb64fc72414b5f
Aleksey Komissarov <ad3002@gmail.com>
2013-10-19 19:12:09 +0400

    fastq_to_fasta wrapper

---
+++ hiseq/fastq_to_fasta.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#@created: 10.10.2013
+#@author: Aleksey Komissarov
+#@contact: ad3002@gmail.com
+
+import sys
+from PyBioSnippets.hiseq.fastq_tools import fastq_to_fasta
+import argparse
+
+if __name__ == '__main__':
+	
+	parser = argparse.ArgumentParser(description='Check presence of adapter kmers.')
+	parser.add_argument('-i','--input', help='Fastq file', required=True)
+	parser.add_argument('-o','--output', help='Fasta output file', required=True)
+	args = vars(parser.parse_args())
+	fastq_to_fasta(args["input"], args["output"])
+	
