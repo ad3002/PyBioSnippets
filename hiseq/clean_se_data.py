1ffdf63f1b1fe8e46bdadbb3748b37ef0c82051a
Aleksey Komissarov <ad3002@gmail.com>
2013-11-04 18:59:17 +0400

    params for se data

--- hiseq/clean_se_data.py
+++ hiseq/clean_se_data.py
@@ -14,13 +14,17 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Check presence of adapter kmers.')
 	parser.add_argument('-p','--prefix', help='SE prefix', required=True)
 	parser.add_argument('-v','--verbose', help='Verbose', required=False, default=False)
+	parser.add_argument('-G','--polyG', help='Length of polyG', required=False, default=23)
+	parser.add_argument('-c','--cutoff', help='Length cutoff', required=False, default=50)
 	parser.add_argument('-a','--adapters', help='File with adapters', required=False, default=None)
 	args = vars(parser.parse_args())
 
 	prefix = args["prefix"]
 	verbose = args["verbose"]
+	cutoff = int(args["cutoff"])
+	polyG_cutoff = int(args["polyG"])
 	adapters_file = args["adapters"]
 	fastq1_file = "%s.fastq" % prefix
 	fastq1ok_file = "%s.ok.fastq" % prefix
 	fastq_bad_file  = "%s.bad.fastq" % prefix
-	clean_single_read_data(fastq1_file, fastq1ok_file, fastq_bad_file, verbose=verbose, adapters_file=adapters_file)
+	clean_single_read_data(fastq1_file, fastq1ok_file, fastq_bad_file, verbose=verbose, adapters_file=adapters_file, cutoff=cutoff, polyG_cutoff=polyG_cutoff)
