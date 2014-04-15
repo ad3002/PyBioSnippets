bde1ecb4171fc711bcfa453f83c9211681ed1a28
Aleksey Komissarov <ad3002@gmail.com>
2014-04-15 13:54:11 +0900

    remove short reads

---
+++ hiseq/fastq_remove_short.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#@created: 10.10.2013
+#@author: Aleksey Komissarov
+#@contact: ad3002@gmail.com
+
+import sys
+from trseeker.seqio.sra_file import fastq_reader
+import argparse
+
+if __name__ == '__main__':
+	
+	parser = argparse.ArgumentParser(description='Remove short reads.')
+	parser.add_argument('-i','--input', help='Fastq file', required=True)
+	parser.add_argument('-o','--output', help='Fastq file', required=True)
+	parser.add_argument('-l','--length', help='Minimal length', required=True)
+	args = vars(parser.parse_args())
+	print "Process data"
+	data = []
+	ok = 0
+	l = int(args["length"])
+	with open(args["output"], "w") as fh:
+		for i, read in enumerate(fastq_reader(args["input"])):
+			if i % 10000 == 0:
+				print ok, i, ok/(i+1.), "\r",
+			if read.length < l:
+				continue
+			fh.write(read.fastq)
+			ok += 1
+	print
+	print "Final: saved %s (%s) from %s" % (ok, ok/(i+1.), i)
+	
